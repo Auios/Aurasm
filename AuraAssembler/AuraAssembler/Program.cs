00b0947abdea096740dac4845f0a043a86aca22a
Auios <LoneAuios@gmail.com>
2018-09-11 15:22:56 -0400

    New AuraAssembler

---
+++ AuraAssembler/AuraAssembler/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuraAssembler
+{
+    class Program
+    {
+        static UInt32 curAdr; // Current address
+        static Hashtable instruction;
+
+        static void Main(string[] args)
+        {
+            
+
+            Parse("JMP start");
+
+            Console.ReadKey();
+        }
+
+        static void SetupInstructionHashTable()
+        {
+            instruction = new Hashtable();
+            instruction["HALT"] = 0;
+            instruction["SYS0"] = 16;
+            instruction["SYS1"] = 17;
+            instruction["AND0"] = 32;
+            instruction["AND1"] = 33;
+            instruction["OR0"] = 34;
+            instruction["OR1"] = 35;
+            instruction["XOR0"] = 36;
+            instruction["XOR1"] = 37;
+            instruction["NOT"] = 38;
+        }
+
+        static void Parse(string line)
+        {
+            line = line.Trim();
+            string[] symbols = line.Split(' ');
+
+            foreach(string symbol in symbols)
+            {
+                Console.WriteLine($"'{symbol}'");
+            }
+        }
+    }
+}
--- AuraAssembler/AuraAssembler/Properties/AssemblyInfo.cs
+++ AuraAssembler/AuraAssembler/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("aurac")]
+[assembly: AssemblyTitle("AuraAssembler")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("aurac")]
+[assembly: AssemblyProduct("AuraAssembler")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("db9a36a3-dafd-429c-b6fa-3d145c38844f")]
+[assembly: Guid("58f3d528-8a56-4f15-b481-34b54cb1b1d7")]
 
 // Version information for an assembly consists of the following four values:
 //
